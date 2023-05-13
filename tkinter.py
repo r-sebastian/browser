5a8e1cc6e116b75103a9181b92895ad395184129
Rohit Sebastian <rohitsebastianp@gmail.com>
2023-05-13 11:38:43 +0530

    can now dispaly webpages BUT formatting is wrong
    when used https and certificare error is shown;Dispaly is not proper can only see a blob

--- tkinter.py
+++ tkinter.py
@@ -1,6 +1,6 @@
 import tkinter
 from browser import request
-WIDTH , HEIGHT = 800 ,600
+WIDTH , HEIGHT = 800 ,600   #resolution of the canvas
 
 def lex(body):
     text = ""
@@ -12,7 +12,8 @@ def lex(body):
             in_angle = False
         elif not in_angle:
             text += c
-    return text
+    return text #returns the content of the page without tags
+
 
 class Browser:
     def __init__(self):
@@ -20,11 +21,11 @@ class Browser:
         self.window = tkinter.Tk()
         #creates the canvas and puts the window inside it
         self.canvas = tkinter.Canvas(
-            self.window, 
+            self.window, #to tell the canvas where to dispaly the canvas
             width=WIDTH,
             height=HEIGHT
         )
-        #pack the canvas to fir tk
+        #pack the canvas to fit tk
         self.canvas.pack()
 
     def load(self, url):
@@ -32,7 +33,7 @@ class Browser:
         headers, body = request(url)
         text = lex(body)
         for c in text:
-            self.canvas.create_text(100, 100, text=c)
+            self.canvas.create_text(100, 100, text=c)   #draw the text char by char
 
 if __name__ == "__main__":
     import sys
